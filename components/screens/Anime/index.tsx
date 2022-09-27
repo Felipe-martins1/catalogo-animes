87bd6c2bc9a31e06ce7331f0d2ffd6f2c77cbf59
Felipe <Felipe.dom.martins@gmail.com>
2022-09-27 12:48:38 -0300

    feat: add meta-tags

--- components/screens/Anime/index.tsx
+++ components/screens/Anime/index.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 import { api } from "../../../utils/api";
 
 import styles from "./Anime.module.scss";
+import Head from "next/head";
 
 const Anime: React.FC = () => {
   const router = useRouter();
@@ -24,8 +25,10 @@ const Anime: React.FC = () => {
   };
 
   useEffect(() => {
-    getAnimeById();
-  }, []);
+    if (id) {
+      getAnimeById();
+    }
+  }, [id]);
 
   const getCanonicalStatus = (status: string) => {
     switch (status) {
@@ -45,59 +48,69 @@ const Anime: React.FC = () => {
   };
 
   return (
-    <Layout>
-      <Breadcrumb>
-        <Breadcrumb.Item>
-          <Link href="/">Home</Link>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item>Anime</Breadcrumb.Item>
-        <Breadcrumb.Item>{anime?.attributes?.canonicalTitle}</Breadcrumb.Item>
-      </Breadcrumb>
+    <React.Fragment>
+      <Head>
+        <title>{anime?.attributes?.canonicalTitle}</title>
 
-      <section className={styles.cardContainer}>
-        <Card hoverable>
-          <div className={styles.banner}>
-            <Image
-              alt={anime?.attributes?.canonicalTitle}
-              src={anime?.attributes?.coverImage?.original}
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-            />
-          </div>
-          <Card.Meta
-            className={styles.meta}
-            title={anime?.attributes.canonicalTitle}
-            description={anime?.attributes.synopsis}
-          />
+        <meta name="og:description" content={anime?.attributes?.description} />
+        <meta name="og:title" content={anime?.attributes?.canonicalTitle} />
+        <meta name="og:image" content={anime?.attributes?.posterImage?.large} />
+      </Head>
 
-          <Divider />
-          <div className={styles.stats}>
-            <Statistic
-              title="Start date"
-              value={anime?.attributes?.startDate}
-            />
-            <Statistic
-              title="Status"
-              value={getCanonicalStatus(anime?.attributes?.status)}
-            />
-            <Statistic
-              title="Popularity Rank"
-              value={anime?.attributes?.popularityRank}
-            />
-            <Statistic
-              title="Average Rating"
-              value={anime?.attributes?.averageRating}
+      <Layout>
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            <Link href="/">Home</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Anime</Breadcrumb.Item>
+          <Breadcrumb.Item>{anime?.attributes?.canonicalTitle}</Breadcrumb.Item>
+        </Breadcrumb>
+
+        <section className={styles.cardContainer}>
+          <Card hoverable>
+            <div className={styles.banner}>
+              <Image
+                alt={anime?.attributes?.canonicalTitle}
+                src={anime?.attributes?.coverImage?.original || ""}
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+              />
+            </div>
+            <Card.Meta
+              className={styles.meta}
+              title={anime?.attributes.canonicalTitle}
+              description={anime?.attributes.synopsis}
             />
 
-            <Statistic
-              title="Age Rating"
-              value={anime?.attributes?.ageRatingGuide}
-            />
-          </div>
-        </Card>
-      </section>
-    </Layout>
+            <Divider />
+            <div className={styles.stats}>
+              <Statistic
+                title="Start date"
+                value={anime?.attributes?.startDate}
+              />
+              <Statistic
+                title="Status"
+                value={getCanonicalStatus(anime?.attributes?.status)}
+              />
+              <Statistic
+                title="Popularity Rank"
+                value={anime?.attributes?.popularityRank}
+              />
+              <Statistic
+                title="Average Rating"
+                value={anime?.attributes?.averageRating}
+              />
+
+              <Statistic
+                title="Age Rating"
+                value={anime?.attributes?.ageRatingGuide}
+              />
+            </div>
+          </Card>
+        </section>
+      </Layout>
+    </React.Fragment>
   );
 };
 
--- components/screens/Home/index.tsx
+++ components/screens/Home/index.tsx
@@ -12,28 +12,38 @@ import { useTrending } from "../../../hooks/useTrending";
 
 import styles from "./Home.module.scss";
 import { findTrendingAnimes } from "../../../services/animeService";
+import React from "react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const { loading: loadingAnimes, data: trendingAnimes } = useTrending();
 
   return (
-    <Layout>
-      <Hero
-        anime={{
-          url: trendingAnimes[0]?.attributes?.coverImage?.large,
-          alt: trendingAnimes[0]?.attributes?.canonicalTitle,
-          title: trendingAnimes[0]?.attributes?.canonicalTitle,
-        }}
-      />
-      <section className={styles.listsContainer}>
-        <AnimeList
-          title="Trending Animes"
-          items={trendingAnimes}
-          loading={loadingAnimes}
+    <React.Fragment>
+      <Head>
+        <title>Anime List</title>
+
+        <meta name="og:description" content="Find your animes in one place!" />
+        <meta name="og:title" content="Anime List" />
+      </Head>
+      <Layout>
+        <Hero
+          anime={{
+            url: trendingAnimes[0]?.attributes?.coverImage?.large,
+            alt: trendingAnimes[0]?.attributes?.canonicalTitle,
+            title: trendingAnimes[0]?.attributes?.canonicalTitle,
+          }}
         />
-      </section>
-      <Search />
-    </Layout>
+        <section className={styles.listsContainer}>
+          <AnimeList
+            title="Trending Animes"
+            items={trendingAnimes}
+            loading={loadingAnimes}
+          />
+        </section>
+        <Search />
+      </Layout>
+    </React.Fragment>
   );
 };
 
